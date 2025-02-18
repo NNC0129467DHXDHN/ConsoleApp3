5b0bbfb03dd47aa0f1d697c1966cb9cd44311ed1
NNC0129467DHXDHN <0129467@st.huce.edu.vn>
2025-02-18 08:00:28 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tamgiac
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("So do canh a la: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("So do canh b la: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("So do canh c la: ");
+            double c = Convert.ToDouble(Console.ReadLine());
+
+            if (a > 0 && b > 0 && c > 0)
+            {
+                if (a + b > c && a + c > b && b + c > a)
+                {
+                    Console.WriteLine("Ba cạnh tạo thành một tam giác.");
+                }
+                else
+                {
+                    Console.WriteLine("Ba cạnh không tạo thành một tam giác.");
+                }
+            }
+
+        }
+    }
+}
+
